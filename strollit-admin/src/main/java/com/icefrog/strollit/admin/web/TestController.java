b0ab81502648c151e92a211d4c55deee0fdf741c
liusuwen <liusuwen@xsyxsc.com>
2019-04-30 10:19:37 +0800

    添加关于rpc和template的test case

--- strollit-admin/src/main/java/com/icefrog/strollit/admin/web/TestController.java
+++ strollit-admin/src/main/java/com/icefrog/strollit/admin/web/TestController.java
@@ -10,6 +10,7 @@ package com.icefrog.strollit.admin.web;
 import com.icefrog.strollit.admin.feign.TestFeign;
 import com.icefrog.strollit.baseframework.api.ApiResult;
 import com.icefrog.strollit.baseframework.web.WebBaseController;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequestMapping("/test")
 @Controller
+@Slf4j
 public class TestController extends WebBaseController {
 
     @Autowired
@@ -26,11 +28,20 @@ public class TestController extends WebBaseController {
     @ResponseBody
     public String go() throws Exception {
         System.out.println("进入go");
+        // 测试远程调用（依赖community服务）
         ApiResult aa = testFeign.aa();
         ApiResult bb = testFeign.bb();
         System.out.println(aa.getMessage());
         System.out.println(bb.getMessage());
         return "success";
+    }
+    
+    @RequestMapping("/testThymeleaf")
+    public String testThymeleaf(){
+        // 测试Thymeleaf模板引擎
+        getRequest().setAttribute("desc", "this is desc");
+        log.info("in testThymeleaf action.");
+        return "template";
     }
 
 
